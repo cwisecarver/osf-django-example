a947c61af6708c70fa7379b6b82ba713c96a9dd4
Chris Wisecarver <cwisecarver@cos.io>
2016-07-19 22:06:28 -0400

    Fix serializers and add admin

--- osf_example_app/zoo/admin.py
+++ osf_example_app/zoo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from zoo import models
+
+admin.register(models.Cat)
+admin.register(models.Dog)
+admin.register(models.Animal)
+admin.register(models.BigCat)
+admin.register(models.Genus)
--- osf_example_app/zoo/serializers.py
+++ osf_example_app/zoo/serializers.py
@@ -11,39 +11,29 @@ class GenusSerializer(serializers.ModelSerializer):
 
 class AnimalSerializer(serializers.ModelSerializer):
     genus = GenusSerializer()
-
+    animals_it_eats = serializers.HyperlinkedRelatedField(
+        many=True,
+        view_name='cat-detail',
+        queryset=models.Animal.objects.all()
+    )
     class Meta:
         model = models.Animal
         fields = ('name', 'number', 'genus', 'animals_it_eats', )
 
 
 class CatSerializer(AnimalSerializer):
-    animals_it_eats = serializers.HyperlinkedRelatedField(
-        many=True,
-        view_name='cat-detail',
-        queryset=models.Cat.objects.all()
-    )
 
     class Meta:
         model = models.Cat
 
 
 class DogSerializer(AnimalSerializer):
-    animals_it_eats = serializers.HyperlinkedRelatedField(
-        many=True,
-        view_name='dog-detail',
-        queryset=models.Animal.objects.all()
-    )
 
     class Meta:
         model = models.Dog
 
 
 class BigCatSerializer(CatSerializer):
-    animals_it_eats = serializers.HyperlinkedRelatedField(
-        many=True,
-        view_name='bigcat-detail',
-        queryset=models.Animal.objects.all()
-    )
+
     class Meta:
         model = models.BigCat
