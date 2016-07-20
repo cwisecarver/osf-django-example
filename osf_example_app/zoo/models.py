2cee659477d66158f88e967c43617736307cdaac
Chris Wisecarver <cwisecarver@cos.io>
2016-07-20 11:34:52 -0400

    Creating fixtures for animals

--- osf_example_app/zoo/models.py
+++ osf_example_app/zoo/models.py
@@ -11,10 +11,12 @@ class Animal(TypedModel):
     number = models.PositiveIntegerField(verbose_name='a name that will appear in the admin')
     genus = models.ForeignKey('Genus')  # putting things in quotes allows you to use it without
                                                              # importing it or before you've defined it
-    animals_it_eats = models.ManyToManyField('self', related_query_name='eaten_by')
+    animals_it_eats = models.ManyToManyField('self', related_query_name='eaten_by', blank=True)
                                                       # ManyToManyField allows relations to multiple things
                                                       # self is a magic keyword that relates it to itself
                                                       # you could also specify an imported model or a model name in quotes
+    def __str__(self):
+        return self.name
 
     def __repr__(self):
         return u'<%s: %s>' % (self.__class__.__name__, self.name)
@@ -25,12 +27,12 @@ class Animal(TypedModel):
 
 class Genus(models.Model):
     name = models.CharField(max_length=255, blank=True)  # use blank=True for optional fields
-
+    def __str__(self):
+        return self.name
 
 class Cat(Animal):
     # because Cat inherits from abstract Animal it will create it's own table with the fields from Animal and from Cat
     loudness = models.IntegerField()
-
 
 class Dog(Animal):
     fuzzy = models.NullBooleanField()
